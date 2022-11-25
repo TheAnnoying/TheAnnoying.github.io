43ab60ac36c6ca338999e103cbd89124a07b92bb
TheAnnoying <drork2018@gmail.com>
2022-11-25 23:18:25 +0200

    Added sidebar

--- index.js
+++ index.js
@@ -1,14 +1,7 @@
 globalThis.E = (e, t) => $(document.createElement(e, t))
 
-$(document).ready(function(){
-    let clicked = false;
-    $(".contact").click(function(){
-        if(!clicked){
-            clicked = true;
-            return $(".contact").removeClass("contact").addClass("discord").html(`TheAnnoying<span>#6068</span>`)
-        } else {
-            clicked = false;
-            return $(".discord").removeClass("discord").addClass("contact").html("Contact me on Discord");
-        }
-    });
-});
+function openNav(){
+    $(".hamburger-bar").toggleClass("active");
+    $(".sidebar").toggleClass("active");
+};
+
